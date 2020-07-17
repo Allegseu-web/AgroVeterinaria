f44b244f403c0c256d021700e81071825dcc1506
syste <syste@DESKTOP-1AUMCNS>
2020-07-16 22:22:10 -0400

    Creamos la entidad Direcciones.

---
+++ Entidades/Direcciones.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgroVeterinaria.Entidades
+{
+    class Direcciones
+    {
+        public int DireccionesId { get; set; }
+        public string Calle { get; set; }
+        public int SuplidorId { get; set; }
+        public string Apartamento { get; set; }
+        public int NumLocalidad { get; set; }
+        public string Provicia { get; set; }
+        public string Municipio { get; set; }
+        public string Sector { get; set; }
+        public string Pais { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public int UsuarioId { get; set; }
+    }
+}

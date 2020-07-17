8e1467295d9f7b2166f747dcc31a10ee4bfc0765
syste <syste@DESKTOP-1AUMCNS>
2020-07-16 20:29:18 -0400

    Creando la entidad Usuarios.

---
+++ Entidades/Usuarios.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AgroVeterinaria.Entidades
+{
+    class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public string Nombres { get; set; }
+        public string Email { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Clave { get; set; }
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+        public string NivelUsuario { get; set; }
+    }
+}

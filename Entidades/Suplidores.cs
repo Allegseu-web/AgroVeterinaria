330d1a26124b1c1093dfa38abe5e130d5e7a5548
syste <syste@DESKTOP-1AUMCNS>
2020-07-16 22:16:47 -0400

    Creamos la entidad Suplidores.

---
+++ Entidades/Suplidores.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AgroVeterinaria.Entidades
+{
+    class Suplidores
+    {
+        [Key]
+        public int SuplidoresId { get; set; }
+        public string Nombre { get; set; }
+        public int DireccionId { get; set; }
+        public string Telefono { get; set; }
+        public string RNC { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public int UsuarioId { get; set; }
+    }
+}

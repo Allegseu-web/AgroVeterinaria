94e99627e7bb26681dc13a858664879816fb0d48
syste <syste@DESKTOP-1AUMCNS>
2020-07-16 21:25:12 -0400

    Creamos la clase Producto con su Detalle.

---
+++ Entidades/ProductosDetalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AgroVeterinaria.Entidades
+{
+    class ProductosDetalle
+    {
+        [Key]
+        public int ProductoDetalleId { get; set; }
+        public int ProductoId { get; set; }
+        public double Cantidad { get; set; }
+        public double Precio { get; set; }
+        public double Importe { get; set; }
+
+        public ProductosDetalle(double cantidad, double precio, double importe)
+        {
+            ProductoDetalleId = 0;
+            Cantidad = cantidad;
+            Precio = precio;
+            Importe = importe;
+        }
+    }
+}

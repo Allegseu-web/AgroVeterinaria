9f181824b79a84f6e04b28d01327f115e39184fc
syste <syste@DESKTOP-1AUMCNS>
2020-07-16 22:13:21 -0400

    Creamos la entidad Compras.

---
+++ Entidades/Compras.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AgroVeterinaria.Entidades
+{
+    class Compras
+    {
+        [Key]
+        public int CompraId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public int SuplidorId { get; set; }
+        public double SubTotal { get; set; }
+        public int UsuarioId { get; set; }
+    }
+}

91d3ca4e6963dd5cc786598f010a992642510e11
syste <syste@DESKTOP-1AUMCNS>
2020-07-16 23:50:40 -0400

    Creamos las BLL.

---
+++ BLL/UsuariosBLL.cs
@@ -0,0 +1,163 @@
+﻿using AgroVeterinaria.DAL;
+using AgroVeterinaria.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AgroVeterinaria.BLL
+{
+    public class UsuariosBLL
+    {
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool esOk = false;
+
+            try
+            {
+                esOk = contexto.Usuarios.Any(e => e.UsuarioId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return esOk;
+        }
+
+        public static bool Guardar(Usuarios usuario)
+        {
+            if (!Existe(usuario.UsuarioId)) { return Insertar(usuario); }
+            else { return Modificar(usuario); } 
+        }
+
+        private static bool Insertar(Usuarios usuario)
+        {
+            Contexto contexto = new Contexto();
+            bool esOk = false;
+
+            try
+            {
+                usuario.Clave = GetSHA256(usuario.Clave);
+                if (contexto.Usuarios.Add(usuario) != null) { esOk = (contexto.SaveChanges() > 0); }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return esOk;
+        }
+
+        private static bool Modificar(Usuarios usuario)
+        {
+            Contexto contexto = new Contexto();
+            bool esOk = false;
+            usuario.Clave = GetSHA256(usuario.Clave);
+            try
+            {
+                contexto.Entry(usuario).State = EntityState.Modified;
+                esOk = (contexto.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return esOk;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool eliminado = false;
+
+            try
+            {
+                var usuario = contexto.Usuarios.Find(id);
+
+                if (usuario != null)
+                {
+                    contexto.Usuarios.Remove(usuario);
+                    eliminado = (contexto.SaveChanges() > 0);
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return eliminado;
+        }
+
+        public static Usuarios Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Usuarios usuario = new Usuarios();
+
+            try
+            {
+                usuario = contexto.Usuarios.Find(id);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return usuario;
+        }
+
+        public static List<Usuarios> GetList(Expression<Func<Usuarios, bool>> usuario)
+        {
+            Contexto contexto = new Contexto();
+            List<Usuarios> Lista = new List<Usuarios>();
+
+            try
+            {
+                Lista = contexto.Usuarios.Where(usuario).ToList();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Lista;
+        }
+
+        private static string GetSHA256(string str)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = sha256.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
+    }
+}

70fc5d6205d8b8983896f64bdd66b596da4f7745
syste <syste@DESKTOP-1AUMCNS>
2020-07-16 20:58:58 -0400

    Creacion del Usuario Administrador desde el OnModelCreating.

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AgroVeterinaria.Entidades;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,9 +8,24 @@ namespace AgroVeterinaria.DAL
 {
     class Contexto : DbContext
     {
+        public DbSet <Usuarios> Usuarios { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source= DATA/DataBase.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuarios>().HasData(new Entidades.Usuarios
+            {
+                UsuarioId = 1,
+                Clave = "HolaMundo",
+                FechaCreacion = DateTime.Now,
+                Nombres = "Admin",
+                NivelUsuario = "Null",
+                Email = "Admin@admin.com",
+                NombreUsuario = "Administrador"
+            });
         }
     }
 }

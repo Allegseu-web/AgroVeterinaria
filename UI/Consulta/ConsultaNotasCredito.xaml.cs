5f91785a1f71f1563fe92f9e0cc3aa6acfa55931
syste <syste@DESKTOP-1AUMCNS>
2020-08-02 17:29:45 -0400

    Creamos las Consultas.

---
+++ UI/Consulta/ConsultaNotasCredito.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AgroVeterinaria.UI.Consulta
+{
+    /// <summary>
+    /// Interaction logic for ConsultaNotasCredito.xaml
+    /// </summary>
+    public partial class ConsultaNotasCredito : Window
+    {
+        public ConsultaNotasCredito()
+        {
+            InitializeComponent();
+        }
+    }
+}
